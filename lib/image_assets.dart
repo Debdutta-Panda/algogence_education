c29a8f06bf4d403f10e0c7301035577aec3ad18b
Debdutta Panda <bedmad82@gmail.com>
2022-07-09 01:38:12 +0530

    090720220138

--- lib/image_assets.dart
+++ lib/image_assets.dart
@@ -2,4 +2,5 @@ class ImageAssets {
   static const String appLogo = "assets/images/app_logo.svg";
   static const String algogenceLogo = "assets/images/algogence_logo.svg";
   static const String homeBanner = "assets/images/undraw_engineering_team_re_fvat.svg";
+  static const String android_kotlin_banner = "assets/images/android_kotlin_banner.png";
 }
